1a13ddd8a0b84339e52109034f8b8d8d44f93c06
Shr3kx <shrey.kuvera@gmail.com>
2026-02-19 22:37:01 +0530

    v 2.3

--- apps/www/components/footer.tsx
+++ apps/www/components/footer.tsx
@@ -3,18 +3,26 @@ export const Footer = () => {
     <div className="w-full">
       <div className="max-w-7xl mx-auto h-16">
         <div className="size-full px-4 md:px-6 flex items-center justify-center prose prose-sm text-sm text-muted-foreground">
-          <p className="text-center truncate">
+          <p className="text-start truncate">
             Built by{' '}
             <a
-              href="https://x.com/imskyleen"
+              href="https://x.com/shr3kxx"
               rel="noopener noreferrer"
               target="_blank"
             >
-              Skyleen
+              Shr3kx
+            </a>{' '}
+            &{' '}
+            <a
+              href="https://x.com/ctrlcat0x"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              iam-sahil
             </a>
             . The source code is available on{' '}
             <a
-              href="https://github.com/imskyleen/animate-ui"
+              href="https://github.com/shr3kx/odysseyUI"
               rel="noopener noreferrer"
               target="_blank"
             >
